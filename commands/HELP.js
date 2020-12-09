1bc726e360a42bf9b95789adbed6c5c71b1bb197
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-09 14:32:48 +0100

    Update HELP.js

--- commands/HELP.js
+++ commands/HELP.js
@@ -3,7 +3,7 @@
 	var fs = require('fs');
 	var commandsList = fs.readFileSync('Storage/commands.txt', 'utf8');
 
-	module.exports.run = async (bot, message, args, servers, joinstatus) => {
+	module.exports.run = async (bot, message, args, servers, server, joinstatus) => {
 		message.channel.send({embed:{
 			description:commandsList,
 			color:0x2471A3
