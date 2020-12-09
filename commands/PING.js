9ddc72015a30d900dc180ef6ebfc2d04979edb3e
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-09 14:33:56 +0100

    Update PING.js

--- commands/PING.js
+++ commands/PING.js
@@ -1,6 +1,6 @@
 	/*jshint esversion: 6 */
 
-	module.exports.run = async (bot, message, args, servers, joinstatus) => {
+	module.exports.run = async (bot, message, args, servers, server, joinstatus) => {
 		message.channel.send({embed:{
 			description:`Ping successful! The bot ${bot.user.tag}! is online!`,
 			color:0x2471A3
