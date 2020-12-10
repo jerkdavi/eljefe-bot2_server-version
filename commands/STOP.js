7c5574db16e1cc5f0812cfeb59c5f3d26827f2ad
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-10 18:37:00 +0100

    Update STOP.js

--- commands/STOP.js
+++ commands/STOP.js
@@ -1,31 +1,35 @@
 	/*jshint esversion: 6 */
 
-	var server;
-
-	module.exports.run = async (bot, message, args, servers, server, joinstatus) => {
-		if(joinstatus==='joined'){
-			console.log('Stop; joinstatus===joined');
+	module.exports.run = async (bot, message, args, servers, server, botStatus) => {
+		//console.log('Step 501');
+		if(botStatus.botstatus===1){
+			//console.log('Step 502');
 			server = servers[message.guild.id];
-			for(var i = server.queue.length -1; i >= 0; i--){
+			//console.log('Step 503');
+			for(let i = server.queue.length -1; i >= 0; i--){
+				//console.log('Step 504');
 				server.queue.splice(i, 1);
 			}
+			//console.log('Step 505');
 			message.channel.send({embed:{
 				description:'Stopping the queue!',
 				color:0x2471A3
 			}});
+			//console.log('Step 506');
 			server.dispatcher.end();
-			console.log('Stop; returning server');
-			return server;
+			//console.log('Step 507');
 		}
 		else{
-			console.log('Stop; joinstatus===waiting');
+			//console.log('Step 508');
 			message.channel.send({embed:{
 				description:'There is nothing to stop!',
 				color:0x2471A3
 			}});
 			return;
 		}
+		//console.log('Step 509');
 	}
+	//console.log('Step 510');
 	module.exports.config = {
 		command:'STOP'
 	}
