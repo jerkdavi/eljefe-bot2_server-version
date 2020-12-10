fa3bb1a68955fdecde65de49956d61983ca3d4d1
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-10 18:36:27 +0100

    Update PLAY.js

--- commands/PLAY.js
+++ commands/PLAY.js
@@ -1,13 +1,16 @@
 	/*jshint esversion: 6 */
 
-	var ytdl = require('ytdl-core');
-
-	module.exports.run = async (bot, message, args, servers, server, joinstatus) => {
-		var sender = message.author;
-		var args = message.content.toString().split(' ');
-		var input = args[0].toUpperCase();
+	module.exports.run = async (bot, message, args, servers, server, botStatus) => {
+		//console.log('Step 301');
+		let ytdl = require('ytdl-core');
+		//console.log('Step 302');
+		let sender = message.author;
+		//console.log('Step 303');
+		let input = args[0].toUpperCase();
+		//console.log('Step 304');
 		
 		if(!args[1]){
+			//console.log('Step 305');
 			message.channel.send({embed:{
 				description:'You need to provide a link!',
 				color:0x2471A3
@@ -15,6 +18,7 @@
 			return;
 		}
 		if(!(ytdl.validateURL(args[1]))){
+			//console.log('Step 306');
 			message.channel.send({embed:{
 				description:'You didn\'t provide a valid youtube link!',
 				color:0x2471A3
@@ -23,6 +27,7 @@
 		}
 		else{
 			if(!message.member.voice.channel){
+				//console.log('Step 307');
 				message.channel.send({embed:{
 					description:'You must be in a voice channel to play the music!',
 					color:0x2471A3
@@ -30,60 +35,79 @@
 				return;
 			}
 			if(!servers[message.guild.id]){
+				//console.log('Step 308');
 				servers[message.guild.id] = {
 					queue: []
 				};
 			}
 
 			server = servers[message.guild.id];
+			//console.log('Step 309');
 			server.queue.push(args[1]);
-				message.channel.send({embed:{
+			//console.log('Step 310');
+			/*message.channel.send({embed:{
 				description:'Song added to the queue!',
 				color:0x2471A3
-			}});
+			}});*/
 
 			function play(connection, message){
-
+				//console.log('Step 311');
 				server = servers[message.guild.id];
+				//console.log('Step 312');
 				server.dispatcher = connection.play(ytdl(server.queue[0], {filter: 'audioonly'}));
+				//console.log('Step 313');
 				server.queue.shift();
+				//console.log('Step 314');
 
-				message.channel.send({embed:{
+				/*message.channel.send({embed:{
 					description:'Playing the song!',
 					color:0x2471A3
-				}});
+				}});*/
 
 				server.dispatcher.on('finish', () => {
+					//console.log('Step 315');
 					if(server.queue[0]){
+						//console.log('Step 316');
 						play(connection, message);
+						//console.log('Step 317');
 					} else {
+						//console.log('Step 318');
 						connection.disconnect();
-						joinstatus='waiting';
-						console.log('Play-Else; joinstatus='+joinstatus);
+						//console.log('Step 319');
+						botStatus.botstatus--;
+						//console.log('Step 320');
 						servers[message.guild.id] = '';
+						//console.log('Step 321');
 					}
+					//console.log('Step 322');
 				});
-				console.log('Play1; joinstatus='+joinstatus);
-				console.log('Play; returning servers, server and joinstatus');
-				return servers, server, joinstatus;
+				//console.log('Step 323');
 			}
+			//console.log('Step 324');
 
-			if(joinstatus==='waiting'){
+			if(botStatus.botstatus===0){
+				//console.log('Step 325');
 				message.member.voice.channel.join().then(function(connection){
-					joinstatus='joined';
-					console.log('Play2; joinstatus='+joinstatus);
+					//console.log('Step 326');
+					botStatus.botstatus++;
+					//console.log('Step 327');
 					play(connection, message);
-					message.channel.send({embed:{
+					//console.log('Step 328');
+					/*message.channel.send({embed:{
 						description:'Joined the voice channel!',
 						color:0x2471A3
-					}});
+					}});*/
+				}).catch(error => {
+					//console.log('Step 329');
+					console.error(error);
 				});
-				console.log('Play3; joinstatus='+joinstatus);
-				console.log('Play; returning joinstatus');
-				return joinstatus;
+				//console.log('Step 330');
 			}
+			//console.log('Step 331');
 		}
+		//console.log('Step 332');
 	}
+	//console.log('Step 333');
 	module.exports.config = {
 		command:'PLAY'
 	}
