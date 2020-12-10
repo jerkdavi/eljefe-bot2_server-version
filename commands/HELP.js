3b3ac5614509accce50bcc0820a8ad8865ab09b5
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-10 22:24:45 +0100

    Update HELP.js

--- commands/HELP.js
+++ commands/HELP.js
@@ -1,20 +1,18 @@
 	/*jshint esversion: 6 */
-
-	module.exports.run = async (bot, message, args, servers, server, botStatus) => {
+	//console.log('Step 100');
+	module.exports.run = async (bot, message, args, Servers, botStatus) => {
 		//console.log('Step 101');
 		let fs = require('fs');
 		//console.log('Step 102');
 		let commandsList = fs.readFileSync('Storage/commands.txt', 'utf8');
 		//console.log('Step 103');
-
-		//console.log('Step 104');
 		message.channel.send({embed:{
 			description:commandsList,
 			color:0x2471A3
 		}});
-		//console.log('Step 105');
+		//console.log('Step 104');
 	}
-	//console.log('Step 106');
+	//console.log('Step 105');
 	module.exports.config = {
 		command:'HELP'
 	}
