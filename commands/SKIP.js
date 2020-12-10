ed4f1eb2442f44e65008894fad4cf5ba308187cf
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-10 18:36:46 +0100

    Update SKIP.js

--- commands/SKIP.js
+++ commands/SKIP.js
@@ -1,28 +1,31 @@
 	/*jshint esversion: 6 */
 
-	var server;
-
-	module.exports.run = async (bot, message, args, servers, server, joinstatus) => {
-		if(joinstatus==='joined'){
-			console.log('Skip; joinstatus===joined');
+	module.exports.run = async (bot, message, args, servers, server, botStatus) => {
+		//console.log('Step 401');
+		if(botStatus.botstatus===1){
+			//console.log('Step 402');
 			server = servers[message.guild.id];
+			//console.log('Step 403');
 			server.dispatcher.end();
+			//console.log('Step 404');
 			message.channel.send({embed:{
 				description:'Skipping the song!',
 				color:0x2471A3
 			}});
-			console.log('Skip; returning server');
-			return server;
+			//console.log('Step 405');
 		}
 		else{
-			console.log('Skip; joinstatus===waiting');
+			//console.log('Step 407');
 			message.channel.send({embed:{
 				description:'There is nothing to skip!',
 				color:0x2471A3
 			}});
+			//console.log('Step 408');
 			return;
 		}
+		//console.log('Step 409');
 	}
+	//console.log('Step 410');
 	module.exports.config = {
 		command:'SKIP'
 	}
