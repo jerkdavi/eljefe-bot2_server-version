2e7099db25307c3802066559a97f657734de8063
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-10 12:38:36 +0100

    Update HELP.js

--- commands/HELP.js
+++ commands/HELP.js
@@ -1,5 +1,8 @@
 	/*jshint esversion: 6 */
 
+	let fs = require('fs');
+	let commandsList = fs.readFileSync('Storage/commands.txt', 'utf8');
+
 	module.exports.run = async (bot, message, args, servers, server, joinstatus) => {
 		message.channel.send({embed:{
 			description:commandsList,
