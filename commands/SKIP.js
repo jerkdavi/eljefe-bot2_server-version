4cb3132f65174742e8754d39772cd514cf8ee8fb
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-14 12:46:59 +0100

    Update SKIP.js

--- commands/SKIP.js
+++ commands/SKIP.js
@@ -2,31 +2,27 @@
 	//console.log('Step 400');
 	module.exports.run = async (bot, message, args, Servers, botStatus) => {
 		//console.log('Step 401');
-		if(botStatus[781277535232458763].botstatus===1){
+		if(botStatus[781277535232458763].botstatus===0){
 			//console.log('Step 402');
-			//let server = Servers[message.guild.id];
-			//console.log('Step 403');
-			Servers[message.guild.id].dispatcher.end();
-			//console.log('Step 404');
-			message.channel.send({embed:{
-				description:'Skipping the song!',
-				color:0x2471A3
-			}});
-			//console.log('Step 405');
-		}
-		//console.log('Step 406');
-		else{
-			//console.log('Step 407');
 			message.channel.send({embed:{
 				description:'There is nothing to skip!',
 				color:0x2471A3
 			}});
-			//console.log('Step 408');
+			//console.log('Step 403');
 			return;
 		}
-		//console.log('Step 409');
+		//console.log('Step 404');
+		//let server = Servers[message.guild.id];
+		//console.log('Step 405');
+		Servers[message.guild.id].dispatcher.end();
+		//console.log('Step 406');
+		message.channel.send({embed:{
+			description:'Skipping the song!',
+			color:0x2471A3
+		}});
+		//console.log('Step 407');
 	}
-	//console.log('Step 410');
+	//console.log('Step 408');
 	module.exports.config = {
 		command:'SKIP'
 	}
