11fbe83ba3f532327742b52240d81f279139c7be
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-14 20:35:28 +0100

    Update eljefe-bot2.js

--- eljefe-bot2.js
+++ eljefe-bot2.js
@@ -22,7 +22,7 @@
 		let jsfiles = files.filter(f => f.split('.').pop() === 'js');
 		//console.log('Step 010');
 		if(jsfiles.length <= 0) { return /*console.log('Step 011'); */console.log('No commands found!'); }
-		if(jsfiles.length > 0) { return /*console.log('Step 012'); */console.log(`${jsfiles.length} commands found!`); }
+		if(jsfiles.length > 0) { /*console.log('Step 012'); */console.log(`${jsfiles.length} commands found!`); }
 		//console.log('Step 013');
 		jsfiles.forEach((f, i) => {
 			//console.log('Step 014');
