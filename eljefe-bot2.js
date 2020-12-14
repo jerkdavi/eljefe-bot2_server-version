478067b10889e5e2dd9f3a36a00c6210fcb00d96
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-14 12:41:00 +0100

    Update eljefe-bot2.js

--- eljefe-bot2.js
+++ eljefe-bot2.js
@@ -22,7 +22,7 @@
 		let jsfiles = files.filter(f => f.split('.').pop() === 'js');
 		//console.log('Step 010');
 		if(jsfiles.length <= 0) { return /*console.log('Step 011'); */console.log('No commands found!'); }
-		else { /*console.log('Step 013'); */console.log(`${jsfiles.length} commands found!`); }
+		if(jsfiles.length > 0) /*console.log('Step 012'); */console.log(`${jsfiles.length} commands found!`); }
 		//console.log('Step 013');
 		jsfiles.forEach((f, i) => {
 			//console.log('Step 014');
@@ -36,7 +36,8 @@
 		//console.log('Step 018');
 	});
 	//console.log('Step 019');
-	let prefix = process.env.prefix;
+	let prefix = '>';
+	//let prefix = process.env.prefix;
 	//console.log('Step 020');
 	let owner = process.env.ownerID;
 	//console.log('Step 021');
@@ -56,44 +57,37 @@
 			//console.log('Step 027');
 		}
 		//console.log('Step 028');
-		if(message.content.startsWith(prefix)){
+		if(!message.content.startsWith(prefix)){
 			//console.log('Step 029');
-			let args = message.content.toString().split(' ');
-			//console.log('Step 030');
-			let input = args[0].toUpperCase();
-			//console.log('Step 031');
-			let cont = input.slice(prefix.length).split(' ');
-			//console.log('Step 032');
-			//let args = cont.slice(1);
-			let cmd = bot.commands.get(cont[0]);
-			//console.log('Step 033');
-			if(cmd) {
-				//console.log('Step 034');
-				cmd.run(bot, message, args, Servers, botStatus);
-				//console.log('Step 035');
-			}
-			//console.log('Step 036');
-			else{
-				//console.log('Step 037');
-				return;
-			}
-			//console.log('Step 038');
-		}
-		//console.log('Step 039');
-		else{
-			//console.log('Step 040');
 			return;
 		}
+		//console.log('Step 030');
+		let args = message.content.toString().split(' ');
+		//console.log('Step 031');
+		let input = args[0].toUpperCase();
+		//console.log('Step 032');
+		let cont = input.slice(prefix.length).split(' ');
+		//console.log('Step 033');
+		//let args = cont.slice(1);
+		let cmd = bot.commands.get(cont[0]);
+		//console.log('Step 034');
+		if(!cmd) {
+			//console.log('Step 035');
+			return;
+		}
+		//console.log('Step 036');
+		cmd.run(bot, message, args, Servers, botStatus);
+		//console.log('Step 037');
+	});
+	//console.log('Step 038');
+	bot.on('ready', function(ready){
+		//console.log('Step 039');
+		console.log(`Logged in as ${bot.user.tag}!`);
+		//console.log('Step 040');
+		bot.user.setStatus('Online');
 		//console.log('Step 041');
 	});
 	//console.log('Step 042');
-	bot.on('ready', function(ready){
-		//console.log('Step 043');
-		console.log(`Logged in as ${bot.user.tag}!`);
-		//console.log('Step 044');
-		bot.user.setStatus('Online');
-		//console.log('Step 045');
-	});
-	//console.log('Step 046');
-	bot.login(process.env.DISCORD_TOKEN);
-	//console.log('Step 047');
+	bot.login('NzgxMjc3NTM1MjMyNDU4NzYz.X77TZw.Bj6AkRIKW_Etti3lhVnAvduWgtU');
+	//bot.login(process.env.DISCORD_TOKEN);
+	//console.log('Step 043');
